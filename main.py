0c324ee666edecacacc8ad9a4e3807f0d1394533
yael-wolff <wolff.yael@gmail.com>
2021-04-11 19:35:54 +0300

    with features

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
 import pandas as pd
 
+features = ["cnt", "t1", "hum", "is_holiday", "season"]
 
+path = 'C:\\Users\Yael\Documents\python\london.csv'
 
-def load_data():
-    df = pd.read_csv("C:\\Users\Yael\Documents\python\london.csv")
-    data = df.to_dict()
+def load_data(path):
+    df = pd.read_csv(path)
+    fea_dic = df[features]
+    data = fea_dic.to_dict(orient='list')
     print(data)
 
-
-load_data()
+load_data(path)
