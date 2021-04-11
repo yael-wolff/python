383765961097daf56830d8ec0d3ebd45c21d3500
yael-wolff <wolff.yael@gmail.com>
2021-04-11 18:44:17 +0300

    statring to implement pandas

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def load_data():
+    df = pd.read_csv("C:\\Users\Yael\Documents\python\london.csv")
+    data = df.to_dict()
+    print(data)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+load_data()
